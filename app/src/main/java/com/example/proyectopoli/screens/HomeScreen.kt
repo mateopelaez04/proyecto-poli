6f4bec95b78460f0e836b1385c33aa2f7964f458
mateo <mpelaezh@poligran.edu.co>
2025-04-06 16:16:08 -0500

    change title

--- app/src/main/java/com/example/proyectopoli/screens/HomeScreen.kt
+++ app/src/main/java/com/example/proyectopoli/screens/HomeScreen.kt
@@ -67,7 +67,7 @@ fun HomeScreen() {
                 TopAppBar(
                     title = {
                         Text(
-                            text = "MiCurriculumYa!",
+                            text = "MiCurriculumYa12!",
                             style = MaterialTheme.typography.titleLarge,
                             fontWeight = FontWeight.Bold,
                             textAlign = TextAlign.Center,
