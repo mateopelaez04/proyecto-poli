2de3616421bb4177269af396cf6b1a74a15429b8
Luis Fernando Peña Garcia <luisferpeg@hotmail.com>
2025-04-09 01:48:53 +1000

    LuisP

--- app/src/main/java/com/example/proyectopoli/screens/fragments/content/FotosFragment.kt
+++ app/src/main/java/com/example/proyectopoli/screens/fragments/content/FotosFragment.kt
@@ -15,7 +15,7 @@ import com.example.proyectopoli.R
 @Composable
 fun FotosFragment() {
     val fotos = listOf(
-        R.drawable.cv_demo_image, // puedes agregar más imágenes aquí si las tienes
+        R.drawable.cv_demo_image, // puedes agregar más imágenes aqui
         R.drawable.poli_1,
         R.drawable.poli_2
     )
