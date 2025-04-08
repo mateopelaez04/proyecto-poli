e328dce26daf3fb1c3a8edd5396ea91c8a090a02
Luis Fernando Peña Garcia <luisferpeg@hotmail.com>
2025-04-09 04:00:00 +1000

    LuisP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-    implementation("io.coil-kt:coil-compose:2.4.0"
+    implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("com.google.accompanist:accompanist-pager:0.32.0")
     implementation("com.google.accompanist:accompanist-pager-indicators:0.32.0")
     implementation(libs.core.ktx)
